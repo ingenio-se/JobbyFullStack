7dea7f2d955b9eb1daf8a7f8e882761a60476690
David Guerra <david.guerra@ingenio-se.com>
2020-09-10 06:18:54 -0500

    20200910-1

---
+++ build/precache-manifest.c49b7d74cae9951727141df10f62a365.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3e8ebbee302e3a91521d075025c22a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "418d9ee0006db9483c6c",
+    "url": "/static/css/main.77b98619.chunk.css"
+  },
+  {
+    "revision": "735acaeb7d50cb724d27",
+    "url": "/static/js/2.3b0fdab7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3b0fdab7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418d9ee0006db9483c6c",
+    "url": "/static/js/main.14172203.chunk.js"
+  },
+  {
+    "revision": "fd1cb9719e685d92264f",
+    "url": "/static/js/runtime-main.e0448d3e.js"
+  },
+  {
+    "revision": "33a6982a60034f9e19dd3ec493624f0c",
+    "url": "/static/media/building.33a6982a.svg"
+  },
+  {
+    "revision": "2bf72ac7f68a480b7ad1597cf463ff92",
+    "url": "/static/media/location.2bf72ac7.svg"
+  },
+  {
+    "revision": "20146c4822b8361a5ee8df2ebc621b50",
+    "url": "/static/media/money.20146c48.svg"
+  },
+  {
+    "revision": "1a2aba9e86b872e69c40da6d6f237348",
+    "url": "/static/media/target.1a2aba9e.svg"
+  }
+]);
--- precache-manifest.c49b7d74cae9951727141df10f62a365.js
+++ precache-manifest.c49b7d74cae9951727141df10f62a365.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f1c9762e080779f19e6b8de99de78e62",
+    "revision": "f3e8ebbee302e3a91521d075025c22a1",
     "url": "/index.html"
   },
   {
-    "revision": "4f91a1b76f56906e96b5",
-    "url": "/static/css/main.1b3cde42.chunk.css"
+    "revision": "418d9ee0006db9483c6c",
+    "url": "/static/css/main.77b98619.chunk.css"
   },
   {
-    "revision": "630d1aad861bbcd5c2fa",
-    "url": "/static/js/2.e843faf6.chunk.js"
+    "revision": "735acaeb7d50cb724d27",
+    "url": "/static/js/2.3b0fdab7.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.e843faf6.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.3b0fdab7.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4f91a1b76f56906e96b5",
-    "url": "/static/js/main.c1edff4e.chunk.js"
+    "revision": "418d9ee0006db9483c6c",
+    "url": "/static/js/main.14172203.chunk.js"
   },
   {
     "revision": "fd1cb9719e685d92264f",
