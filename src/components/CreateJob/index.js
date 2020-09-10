472d8f89b06c92999e402d1873b4de26b8a7ed99
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-09-09 22:46:42 -0500

    UX introduced + Form Views improved

--- src/components/CreateJob/index.js
+++ src/components/CreateJob/index.js
@@ -1,135 +1,135 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./style/index.scss";
-import axios from 'axios';
+import axios from "axios";
 
 export default class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
-      location: '',
-      company: '',
-      salary:'',
-      ratio:'',
-      description:''
-   }
-   
-   this.handleChange = this.handleChange.bind(this);
-   this.handleSubmit = this.handleSubmit.bind(this);
+      title: "",
+      location: "",
+      company: "",
+      salary: "",
+      ratio: "",
+      description: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  
+
   handleChange({ target }) {
     this.setState({
-        [target.id]: target.value
+      [target.id]: target.value,
     });
     //console.log(this.state)
   }
-  handleSubmit(ev){
+  handleSubmit(ev) {
     ev.preventDefault();
-      console.log(this.state);
-      const data = new FormData();
-      data.append('title', this.state.title);
-      data.append('location', this.state.location);
-      data.append('company', this.state.company);
-      data.append('salary', this.state.salary);
-      data.append('ratio', this.state.ratio);
-      data.append('description', this.state.description);
-  
-      fetch('/createJob', {
-        method: 'POST',
-        body: data,
-      }).then((response) => response.json()).then((responseJson) => {
-          alert(responseJson);
-       
+    console.log(this.state);
+    const data = new FormData();
+    data.append("title", this.state.title);
+    data.append("location", this.state.location);
+    data.append("company", this.state.company);
+    data.append("salary", this.state.salary);
+    data.append("ratio", this.state.ratio);
+    data.append("description", this.state.description);
+
+    fetch("/createJob", {
+      method: "POST",
+      body: data,
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        alert(responseJson);
       });
   }
-  
+
   render() {
     return (
-      <div className="form-container">
-        <div className="form-content">
-          <form onSubmit={this.handleSubmit}> 
-            <h2>Create Vacancy</h2>
+      <div className="view-container">
+        <div className="form-container">
+          <div className="form-content">
+            <form onSubmit={this.handleSubmit}>
+              <h2>Create Vacancy</h2>
 
-            <div className="form-group">
-              <label>Job Title</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="First name"
-                id="title"
-                onChange = {this.handleChange}
-              />
-            </div>
+              <div className="form-group">
+                <label>Job Title</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  id="title"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-            <div className="form-group">
-              <label>Location</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Location..."
-                id="location"
-                onChange = {this.handleChange}
-              />
-            </div>
+              <div className="form-group">
+                <label>Location</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Location..."
+                  id="location"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-            <div className="form-group">
-              <label>Company</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Company Name..."
-                id="company"
-                onChange = {this.handleChange}
-              />
-            </div>
+              <div className="form-group">
+                <label>Company</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Company Name..."
+                  id="company"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-            <div className="form-group">
-              <label>Salary Rank</label>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="$37K-$66K (Glassdoor est.)"
-                id="salary"
-                onChange = {this.handleChange}
-              />
-            </div>
+              <div className="form-group">
+                <label>Salary Rank</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="$37K-$66K (Glassdoor est.)"
+                  id="salary"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-            <div className="form-group">
-              <label>Ratio (0-10)</label>
-              <input
-                type="number"
-                className="form-control"
-                min="0"
-                max="10"
-                id="ratio"
-                onChange = {this.handleChange}
-              />
-            </div>
+              <div className="form-group">
+                <label>Ratio (0-10)</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  min="0"
+                  max="10"
+                  id="ratio"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-            <div className="form-group">
-              <label>Description</label>
-              <textarea
-                type="text"
-                className="form-control"
-                placeholder="Job Description..."
-                id="description"
-                onChange = {this.handleChange}
-              />
-            </div>
+              <div className="form-group">
+                <label>Description</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  placeholder="Job Description..."
+                  id="description"
+                  onChange={this.handleChange}
+                />
+              </div>
 
-         
-          
               <button
                 type="submit"
                 className="btn btn-primary btn-block"
                 id="create-job-btn"
-            >
+              >
                 Create
               </button>
-          
-          </form>
+            </form>
+          </div>
         </div>
       </div>
     );
--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -3,127 +3,129 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 // import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 //
-import axios from 'axios';
+import axios from "axios";
 
 export default class index extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      first_name :'',
-      last_name:'',
-      email: '',
-      password: '',
-   }
-   this.registerUser = this.registerUser.bind(this);
-   this.handleChange = this.handleChange.bind(this);
-   this.handleSubmit = this.handleSubmit.bind(this);
+      first_name: "",
+      last_name: "",
+      email: "",
+      password: "",
+    };
+    this.registerUser = this.registerUser.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   static propTypes = {
     prop: PropTypes,
   };
   handleChange({ target }) {
     this.setState({
-        [target.id]: target.value
+      [target.id]: target.value,
     });
     //console.log(this.state)
-}
-handleSubmit(ev){
-  ev.preventDefault();
+  }
+  handleSubmit(ev) {
+    ev.preventDefault();
 
     const data = new FormData();
-    data.append('first_name', this.state.first_name);
-    data.append('last_name', this.state.last_name);
-    data.append('email', this.state.email);
-    data.append('password', this.state.password);
+    data.append("first_name", this.state.first_name);
+    data.append("last_name", this.state.last_name);
+    data.append("email", this.state.email);
+    data.append("password", this.state.password);
 
-    fetch('registerUser', {
-      method: 'POST',
+    fetch("registerUser", {
+      method: "POST",
       body: data,
-    }).then((response) => response.json()).then((responseJson) => {
-      alert(responseJson);
-      
-    });
-}
-registerUser(){
-    let url = 'registerUser'
-    console.log(this.state)
-    axios.post(url, { datos: this.state })
-      .then(resp => {
-        console.log(resp.data)
-        alert(resp.data)
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        alert(responseJson);
+      });
+  }
+  registerUser() {
+    let url = "registerUser";
+    console.log(this.state);
+    axios
+      .post(url, { datos: this.state })
+      .then((resp) => {
+        console.log(resp.data);
+        alert(resp.data);
       })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   render() {
     return (
-      <div className="form-container">
-        <div className="form-content">
-        <form onSubmit={this.handleSubmit}>
-          <h3>Sign Up</h3>
+      <div className="view-container">
+        <div className="form-container">
+          <div className="form-content">
+            <form onSubmit={this.handleSubmit}>
+              <h3>Sign Up</h3>
 
-          <div className="form-group">
-            <label>First name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="First name"
-              id="first_name"
-              onChange = {this.handleChange}
-            />
+              <div className="form-group">
+                <label>First name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="First name"
+                  id="first_name"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Last name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Last name"
+                  id="last_name"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  id="email"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                  id="password"
+                  onChange={this.handleChange}
+                />
+              </div>
+
+              <Link to={{ pathname: "/job/create", state: { texto: "Skip" } }}>
+                <button
+                  type="submit"
+                  className="btn btn-primary btn-block"
+                  id="register-btn"
+                >
+                  Sign Up
+                </button>
+              </Link>
+
+              <p className="forgot-password text-right">
+                Already registered? <Link to={`/login`}>sign in</Link>
+              </p>
+            </form>
           </div>
-
-          <div className="form-group">
-            <label>Last name</label>
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Last name"
-              id="last_name"
-              onChange = {this.handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Email address</label>
-            <input
-              type="email"
-              className="form-control"
-              placeholder="Enter email"
-              id="email"
-              onChange = {this.handleChange}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Password</label>
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Enter password"
-              id="password"
-              onChange = {this.handleChange}
-            />
-          </div>
-
-          
-            <button
-              type="submit"
-              className="btn btn-primary btn-block"
-              id="register-btn"
-            
-            >
-              Sign Up
-            </button>
-       
-          <Link className="text-reset text-decoration-none" to={`/login`}>
-            <p className="forgot-password text-right">
-              Already registered <a href="#">sign in?</a>
-            </p>
-          </Link>
-        </form>
-      </div>
+        </div>
       </div>
     );
   }
