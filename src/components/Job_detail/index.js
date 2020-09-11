ed8d4772faaf1f4dc97e4861743935a0fd3baba7
Daniel Arrubla <darrubla@uninorte.edu.co>
2020-09-11 18:46:31 -0500

    suggested changes

--- src/components/Job_detail/index.js
+++ src/components/Job_detail/index.js
@@ -22,78 +22,74 @@ class index extends Component {
   static propTypes = {
     prop: PropTypes,
   };
-  componentDidMount() {
-    const children = this.props.children;
-    fetch(api(children)).then(
-      (result) => {
-        console.log(result);
-        this.setState({
-          isLoaded: true,
-          data: result,
-        });
-      },
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error,
-        });
-      }
-    );
-  }
+  // componentDidMount() {
+  //   const children = this.props.children;
+  //   fetch(api(children)).then(
+  //     (result) => {
+  //       console.log(result);
+  //       this.setState({
+  //         isLoaded: true,
+  //         data: result,
+  //       });
+  //     },
+  //     (error) => {
+  //       this.setState({
+  //         isLoaded: true,
+  //         error,
+  //       });
+  //     }
+  //   );
+  // }
   render() {
-    const { error, isLoaded } = this.state;
-    const { url } = this.state.data;
-    if (error) {
-      console.log(error.message);
-      return <h1>Oops, data no disponible</h1>;
-    } else if (!isLoaded) {
-      return <h1>Loading...</h1>;
-    } else {
-      return (
-        <div className="job_container">
-          <div>
-            <article>
-              <h2>{this.state.job[0][0]}</h2>
-              <img src={url} alt="logo" />
-            </article>
+    return (
+      <div className="job_container">
+        <div>
+          <article>
+            <h2>{this.state.job[0][0]}</h2>
+            <img src={url} alt="logo" />
+          </article>
 
-            <section className="card-aspects__container">
-              <ul>
-                <li>
-                  <span>
-                    <img src={location} alt="location" /> {this.state.job[0][1]}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    <img src={building} alt="building" /> {this.state.job[0][2]}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    <img src={money} alt="money" /> {this.state.job[0][3]}
-                  </span>
-                </li>
-                <li>
-                  <span>
-                    <img src={target} alt="target" /> {this.state.job[0][4]}
-                  </span>
-                </li>
-              </ul>
-            </section>
-            <section className="scrollable">
-              <p>{this.state.job[0][5]}</p>
-              <section className="buttons">
-                <button className="apply">Apply</button>
-                <Link className="text-reset text-decoration-none" to={`/`}>
-                  <button className="back">Back</button>
-                </Link>
-              </section>
-            </section>
-          </div>
+          <section className="card-aspects__container">
+            <ul>
+              <li>
+                <span>
+                  <img src={location} alt="location" /> {this.state.job[0][1]}
+                </span>
+              </li>
+              <li>
+                <span>
+                  <img src={building} alt="building" /> {this.state.job[0][2]}
+                </span>
+              </li>
+              <li>
+                <span>
+                  <img src={money} alt="money" /> {this.state.job[0][3]}
+                </span>
+              </li>
+              <li>
+                <span>
+                  <img src={target} alt="target" /> {this.state.job[0][4]}
+                </span>
+              </li>
+            </ul>
+          </section>
+          <p>{this.state.job[0][5]}</p>
+          <section className="tag-container">
+            <span>1</span>
+            <span>databases</span>
+            <span>machine learning</span>
+            <span>4</span>
+            <span>5</span>
+          </section>
+          <section className="buttons">
+            <button className="apply">Apply</button>
+            <Link className="text-reset text-decoration-none" to={`/`}>
+              <button className="back">Back</button>
+            </Link>
+          </section>
         </div>
-      );
-    }
+      </div>
+    );
   }
 }
 export default index;
--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,68 +1,72 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./style/index.scss";
-import axios from 'axios';
+import axios from "axios";
 
 class index extends Component {
   constructor(props) {
     super(props);
-   // console.log(props)
+    // console.log(props)
     this.state = {
-       salary:'',
-    }
+      salary: "",
+    };
     // bind
-    
+
     this.handleChange = this.handleChange.bind(this);
     //this.search = this.search.bind(this);
-    
   }
 
   static propTypes = {
     prop: PropTypes,
   };
   handleChange({ target }) {
-    console.log(target.value)
-    let url = '/salary/'+target.value
-    axios.get(url)
-      .then(resp => {
-          console.log(resp.data);
+    console.log(target.value);
+    let url = "/salary/" + target.value;
+    axios
+      .get(url)
+      .then((resp) => {
+        console.log(resp.data);
 
-          this.setState({
-              jobs: resp.data,
-          });
-          
-          this.props.handleJobs(this.state.jobs);
+        this.setState({
+          jobs: resp.data,
+        });
+
+        this.props.handleJobs(this.state.jobs);
       })
-      .catch(err => {
-          console.log(err)
-      })
-  
+      .catch((err) => {
+        console.log(err);
+      });
   }
   render() {
     return (
       <div className="sidebar__container">
-        <h3>Salary Rank</h3>
-        <ul>
-         
-          <li>
-            <input type="checkbox" name="salary" id="" value="25-40" onClick={this.handleChange}/> 25k - 40k
-          </li>
-          <li>
-            <input type="checkbox" name="salary" id="" value="40-60" onClick={this.handleChange} />40k - 60K
-          </li>
-          <li>
-            <input type="checkbox" name="salary" id="" value="60-80" onClick={this.handleChange}/> 60k - 80k
-          </li>
-          <li>
-            <input type="checkbox" name="salary" id="" value="80-100" onClick={this.handleChange}/>80k - 100k
-          </li>
-          <li>
-            <input type="checkbox" name="salary" id="" value="100-180" onClick={this.handleChange}/>100k - 180k
-          </li>
-          <li>
-            <input type="checkbox" name="salary" id="" value="120-180" onClick={this.handleChange}/>120k - 180k
-          </li>
-        </ul>
+        {/* <h3>Salary Rank</h3> */}
+        <form>
+          <div data-role="rangeslider">
+            <label for="range-1a">Salary Rank</label>
+            <input
+              type="range"
+              name="range-1a"
+              id="range-1a"
+              min="0"
+              max="130"
+              value="30"
+              // data-popup-enabled="true"
+              // data-show-value="true"
+            ></input>
+            <label for="range-1b">Salary Rank</label>
+            <input
+              type="range"
+              name="range-1b"
+              id="range-1b"
+              min="0"
+              max="130"
+              value="90"
+              // data-popup-enabled="true"
+              // data-show-value="true"
+            ></input>
+          </div>
+        </form>
       </div>
     );
   }
